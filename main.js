43ac1f3f1c8cebc40bb441058c7fd14ba817f419
torontocaper <torontocaper@gmail.com>
2022-04-07 20:48:44 -0400

    added canvas element

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function clickButton() {
     document.getElementById("test").remove();
 }
+function drawRect() {
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
+    context.fillStyle = 'green';
+    context.fillRect(10, 10, 150, 100);
+}
