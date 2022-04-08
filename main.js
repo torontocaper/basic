1ce2ff52dedb69595082431677891f866e568b10
torontocaper <torontocaper@gmail.com>
2022-04-07 21:18:36 -0400

    added another button to 'create' a canvas element

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ function drawRect() {
     context.fillStyle = 'green';
     context.fillRect(10, 10, 150, 100);
 }
+
+function drawNewRect() {
+    let canvasElement = document.createElement("canvas");
+    let canvasContext = canvasElement.getContext("2d")
+    canvasContext.fillStyle = "green";
+    canvasContext.fillRect(10, 10, 50, 50);
+    
+    let placement = document.getElementById("button2")
+
+    document.body.insertBefore(canvasElement, placement);
+}
